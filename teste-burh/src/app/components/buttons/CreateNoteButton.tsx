0c20edb8a8ee6a0c043a8393366ef3952e12a07a
iagop <iagopuzer06@hotmail.com>
2024-06-09 15:53:05 -0300

    Criando o component de button

---
+++ teste-burh/src/app/components/buttons/CreateNoteButton.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  onCreate: () => void;
+}
+
+export default function CreateNoteButton({ onCreate }: Props): JSX.Element {
+  return (
+    <button
+      onClick={onCreate}
+      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+    >
+      Criar nota
+    </button>
+  );
+}
